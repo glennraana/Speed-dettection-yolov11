bd0266470e34e4e1c9b9b0be56d2d491c23a28b4
Glenn Råna <79666227+glennraana@users.noreply.github.com>
2024-10-17 11:20:07 +0200

    Add files via upload

---
+++ estimate_speed_yolo11.py
@@ -0,0 +1,37 @@
+import cv2
+from ultralytics import YOLO, solutions
+
+model = YOLO("yolo11n.pt")
+names = model.model.names
+
+cap = cv2.VideoCapture("/Users/glenn/estimate_speed/estimate_speed_yolo/4261469-uhd_2560_1440_25fps.mp4")
+assert cap.isOpened(), "Error reading video file"
+w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+
+# Video writer
+video_writer = cv2.VideoWriter("speed_estimation.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
+
+# line_pts where we want to set the line for speed detection
+line_pts = [(300, 1000), (2500, 1000)]  # Example values
+
+# Init speed-estimation obj
+speed_obj = solutions.SpeedEstimator(
+    reg_pts=line_pts,
+    names=names,
+    view_img=True,
+)
+
+while cap.isOpened():
+    success, im0 = cap.read()
+    if not success:
+        print("Video frame is empty or video processing has been successfully completed.")
+        break
+
+    tracks = model.track(im0, persist=True)
+
+    im0 = speed_obj.estimate_speed(im0, tracks)
+    video_writer.write(im0)
+
+cap.release()
+video_writer.release()
+cv2.destroyAllWindows()
